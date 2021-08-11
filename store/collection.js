963ec8d73b208e79a6e3d2265f2509efdb5ae409
kalys <kalys.khairy@klikacc.com>
2021-08-11 22:11:32 +0700

    first commit

---
+++ store/collection.js
@@ -0,0 +1,64 @@
+import Repository, {
+    serializeQuery,
+    baseUrl
+} from '~/repositories/Repository.js';
+
+export const state = () => ({
+    collections: null,
+    categories: null,
+    queries: null
+});
+
+export const mutations = {
+    setCollections(state, payload) {
+        state.collections = payload;
+    },
+
+    setCategories(state, payload) {
+        state.categories = payload;
+    },
+    setQueries(state, payload) {
+        state.queries = payload;
+    }
+};
+
+export const actions = {
+    async getCollectionsBySlugs({ commit }, payload) {
+        console.log('ini payload collectionjs ', payload);
+        console.log('ini commit collectionjs ', commit);
+        let query = '';
+        payload.forEach(item => {
+            if (query === '') {
+                query = `slug_in=${item}`;
+            } else {
+                query = query + `&slug_in=${item}`;
+            }
+        });
+        const reponse = await Repository.get(`${baseUrl}/collections?${query}`)
+            .then(response => {
+                commit('setCollections', response.data);
+                return response.data;
+            })
+            .catch(error => ({ error: JSON.stringify(error) }));
+        return reponse;
+    },
+    async getCategoriesBySlugs({ commit }, payload) {
+        let query = '';
+        payload.forEach(item => {
+            if (query === '') {
+                query = `slug_in=${item}`;
+            } else {
+                query = query + `&slug_in=${item}`;
+            }
+        });
+        const reponse = await Repository.get(
+            `${baseUrl}/product-categories?${query}`
+        )
+            .then(response => {
+                commit('setCategories', response.data);
+                return response.data;
+            })
+            .catch(error => ({ error: JSON.stringify(error) }));
+        return reponse;
+    }
+};
