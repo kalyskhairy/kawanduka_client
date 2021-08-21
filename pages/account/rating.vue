99b2c7a819885bea5e1518e10fede3b7ee2287c1
kalys <kalys.khairy@klikacc.com>
2021-08-21 23:41:49 +0700

    UI rating page

--- pages/account/rating.vue
+++ pages/account/rating.vue
@@ -2,20 +2,69 @@
     <section class="ps-page--my-account">
         <bread-crumb :breadcrumb="breadCrumb" />
         <!-- <user-information /> -->
-        <div class="row rem">
-            <div class="col-lg-12 col-md-12 col-sm-12">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-lg-12 col-md-12">
-                                <h3>Mohon berikan rating produk atau jasa sesuai tingkat kepuasan Anda.</h3>
-                                <rating />
-                                <v-textarea
-                                filled
-                                name="input-7-4"
-                                label="Filled textarea"
-                                value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
-                                ></v-textarea>
+        <div class="ps-page__content">
+            <div class="row mt-10 mr-10 ml-10">
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-lg-12 col-md-12">
+                                    <div class="mt-10 mb-10">
+                                        <div class="row">
+                                            <div class="col-lg-12 col-md-12 text-center">
+                                                <h3>Mohon berikan rating produk atau jasa sesuai tingkat kepuasan Anda.</h3>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-12 col-md-12 text-center">
+                                                <rating />
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-12 col-md-12">
+                                                <label>Tulis Ulasan Anda</label>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-12 col-md-12 text-center">
+                                                <v-textarea
+                                                filled
+                                                name="input-7-4"
+                                                label="Filled textarea"
+                                                value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
+                                                ></v-textarea>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-12 col-md-12">
+                                                <label>Tambahkan Foto</label>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-12 col-md-12 text-center">
+                                                <v-file-input
+                                                ></v-file-input>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-12 col-md-12 text-center mb-2">
+                                                <v-btn
+                                                elevation="2"
+                                                class="btn-modal"
+                                                style="min-width:30%;"
+                                                >Kirim</v-btn>
+                                            </div>
+                                            <div class="col-lg-12 col-md-12 text-center">
+                                                <v-btn
+                                                style="min-width:30%;"
+                                                color="secondary"
+                                                elevation="2"
+                                                outlined
+                                                >Batalkan</v-btn>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -60,5 +109,12 @@ export default {
 <style lang="scss" scoped>
 .rem {
     margin: 10rem;
+}
+.btn-modal {
+    background-color: #7aD47C !important;
+    color: white;
+}
+.btn-block {
+    height: 40px;
 }
 </style>
